ce133c1e84de3256ddbdfb74630aaa8b091a9227
Valeriia Lytvynova <lyt.valery3340@gmail.com>
2022-09-05 14:13:40 +0300

    Added url in setup.py

--- HW_7/clean_folder/setup.py
+++ HW_7/clean_folder/setup.py
@@ -6,7 +6,7 @@ setup(
     description='Folder parser',
     author='Valeriia Lytvynova',
     author_email='lyt.valery3340@gmail.com',
-    url='',
+    url='https://github.com/ValeryLyt/HW.GoIt/blob/main/HW_7/clean_folder/setup.py',
     license='',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean-folder=clean_folder.clean:main']},
